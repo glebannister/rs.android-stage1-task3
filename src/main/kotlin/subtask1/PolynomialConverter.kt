84344d07933f094ff8ba8c36439c3229cd8b6443
Gleb <igi2000@inbox.ru>
2020-05-06 12:05:34 +0300

    feat: new PolynomialConverter implementation

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,10 +1,36 @@
 package subtask1
 
+import kotlin.math.absoluteValue
+
 class PolynomialConverter {
 
 
     fun convertToStringFrom(numbers: Array<Int>): String? {
         if (numbers.isEmpty()) return null
+        var result = ""
+        val coef = numbers.size - 1
+        for (n in numbers.indices) {
+            if (numbers[n] == 0) {
+                result += ""
+            } else if (numbers[n] == -1) {
+                result += " - " + "x"
+            } else if (numbers[n] == 1) {
+                result += " + " + "x"
+            } else if (numbers[n] > 0) {
+                if (n == coef) {
+                    result += " + " + numbers[n].absoluteValue.toString()
+                } else if (n == coef - 1) {
+                    result += " + " + numbers[n].absoluteValue.toString() + "x"
+                } else if (numbers[n] == numbers.first()) {
+                    result += numbers[n].absoluteValue.toString() + "x^" + (coef).toString()
+                } else {
+                    result += " + " + numbers[n].absoluteValue.toString() + "x^" + (coef - n).toString()
+                }
+            } else if (numbers[n] < 0) {
+                result += " - " + numbers[n].absoluteValue.toString() + "x^" + (coef - n).toString()
+            }
 
+        }
+        return result
     }
 }
